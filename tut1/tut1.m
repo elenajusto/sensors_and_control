2596ad082515a14786dfc3e755c11829f0a07095
Elena <56148816+elenajusto@users.noreply.github.com>
2024-08-06 16:15:00 +1000

    initial upload of working files

---
+++ tut1/tut1.m
@@ -0,0 +1,8 @@
+% Load image
+imgUTS = imread("datasets/image1.png");
+imgFruit = imread("datasets/image2.png");
+
+imshowpair(imgUTS, imgFruit);               % See images
+
+disp(size(imgUTS))                          % Image matrix
+
