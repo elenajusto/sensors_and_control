171718aded863b7576dd1b9de8dffbd205e4162d
Elena <56148816+elenajusto@users.noreply.github.com>
2024-08-10 10:26:51 +1000

    tut1.m: refactored comments to be more explanatory

--- tut1/tut1.m
+++ tut1/tut1.m
@@ -5,16 +5,16 @@ disp("Size of image:")
 disp(size(imgFruit));                     % Size of image array
 
 disp("Dimensions of image array: ")
-disp(ndims(imgFruit))
+disp(ndims(imgFruit))                     % Number of dimensions
 
-%imshow(imgFruit(:,:))
+% Convert image to grayscale
+imgFruitGray = rgb2gray(imgFruit);        
 
-%imshow(rgb2gray(imgFruit))
-
-imgFruitGray = rgb2gray(imgFruit);
-
+% Convert image to uint8 - Not sure why?
 imgFruitGray = im2uint8(imgFruitGray);
 
+% Binarize image - Not sure what threshold?
 imgFruitBinary = imbinarize(imgFruitGray);
 
+% Display grayscaled image and binarized image
 imshowpair(imgFruitGray, imgFruitBinary, "montage")
